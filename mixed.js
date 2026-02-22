537d152021b9b8fa43210e1d5dc830264bcacbb7
SumanrajBera <srbera17@gmail.com>
2026-02-22 19:55:36 +0530

    Last 10 remain

---
+++ mixed.js
@@ -0,0 +1,136 @@
+// 81. Write a function that takes an array and returns a new array containing only the even numbers from the original array.
+function filterEven(arr) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) res.push(arr[i])
+    }
+    return res
+}
+
+console.log(filterEven([1, 2, 3, 4, 5, 6, 7, 8]))
+
+
+// 82. Create a function that counts how many times a specific value appears in an array.
+function countApp(arr, k) {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === k) count++
+    }
+    return count
+}
+
+console.log(countApp([1, 2, 3, 2, 5, 6, 2, 2], 2))
+
+// 83. Write a function that takes a string as input and prints the reversed string.
+function revStr(str) {
+    let res = ""
+    for (let i = 0; i < str.length; i++) {
+        res = str.charAt(i) + res
+    }
+    return res
+}
+
+console.log(revStr("Hello World"))
+
+// 84. Create a function that prints a right-angled triangle pattern of asterisks (`*`) based on a given number.
+let height = 5
+for (let i = 0; i < height; i++) {
+    let res = ""
+    for (let j = 0; j <= i; j++) {
+        res += "* "
+    }
+    console.log(res)
+}
+
+// 85. Write a function that takes an array of numbers and returns a new array where each number is squared.
+function squareElem(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] *= arr[i]
+    }
+    console.log(arr)
+}
+squareElem([1, 3, 2, 5, 4])
+
+// 86. Using a loop, calculate and print the sum of all odd numbers between 1 and 50.
+function filterSumOdd(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0) sum += arr[i]
+    }
+    return sum
+}
+
+console.log(filterSumOdd([1, 2, 3, 4, 5, 6, 7, 8]))
+
+// 87. Create an object representing a person with first and last name, and print the full name by combining both values.
+let objPerson = {
+    fname: "Samuel",
+    lname: "Jackson"
+}
+console.log(objPerson.fname + " " + objPerson.lname)
+
+// 88. Convert a numeric string to a number using `parseInt()` and add 5 to it. Print the result.
+let numStr = "1023"
+let convert = parseInt(numStr) + 5
+console.log(convert);
+
+// 89. Reverse an array manually (without using `.reverse()`) and print the reversed array.
+function revArr(arr) {
+    let res = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        res.push(arr[i])
+    }
+    return res
+}
+
+console.log(revArr([1, 2, 3, 4, 5, 6, 7, 8]))
+
+
+// 90. Write a program that checks whether an array is empty. Print "array is empty" or "array is not empty".
+function checkEmpty(arr) {
+    if (arr.length === 0) console.log("Empty");
+    else console.log("Not Empty");
+}
+let arr1 = []
+let arr2 = [1, 2, 3, 4]
+checkEmpty(arr1)
+checkEmpty(arr2)
+
+// 91. Get the current date and format it as `DD/MM/YYYY`. Print both the original `Date` object and the formatted date.
+let currDate = new Date()
+console.log(currDate.toLocaleDateString("en-GB"))
+
+// 92. Using `Math.min()` and the spread operator, find and print the smallest number in an array.
+let arrEx = [10, 2, 5, -1, 30]
+console.log(Math.min(...arrEx))
+
+// 93. Write a function that returns the first `n` numbers of the Fibonacci sequence.
+let n = 10;
+function fibonacci(n) {
+    let a = 0;
+    let b = 1;
+    if (n === 1) {
+        console.log("0")
+        return
+    }
+    if (n === 2) {
+        console.log("0 1")
+        return
+    }
+    n -= 2
+    // for(let i = 1)
+}
+
+// 94. Write a function that divides two numbers. If the second number is zero, throw and catch an error saying "Cannot divide by zero".
+
+// 95. Write a function that returns the index of the first vowel in a string. If no vowel exists, return `-1`.
+
+// 96. Write a function that removes duplicate values from an array and returns the unique values.
+
+// 97. Write a function that merges two sorted arrays into a single sorted array.
+
+// 98. Create a function that returns the total number of characters in a string.
+
+// 99. Access a button with the ID `toggleButton` and add a click event listener that toggles the button’s background color between blue and red.
+
+// 100. Write a function that checks whether all numbers in an array are greater than a given value using the `.every()` method.
